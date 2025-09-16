ec6ed05fe036fafa4246927437679499210be41d
Jeonghoon Park <hoonably@gmail.com>
2025-09-17 03:24:35 +0900

    .

--- src/eval.sh
+++ src/eval.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ "$1" == "0" ]; then
-    for r in $(seq 0.10 0.05 0.50); do
+    # Stage 1 Resize ratio: 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50
+    for r in $(seq 0.20 0.05 0.50); do
         python run_gui_actor.py $1 --r $r --th 0.03 --p 0
         python run_gui_actor.py $1 --r $r --th 0.03 --p 10
         python run_gui_actor.py $1 --r $r --th 0.03 --p 20
@@ -11,7 +12,7 @@ if [ "$1" == "0" ]; then
     done
 
 elif [ "$1" == "1" ]; then
-    for r in $(seq 0.10 0.05 0.50); do
+    for r in $(seq 0.20 0.05 0.50); do
         python run_gui_actor.py $1 --r $r --th 0.05 --p 0
         python run_gui_actor.py $1 --r $r --th 0.05 --p 10
         python run_gui_actor.py $1 --r $r --th 0.05 --p 20
@@ -21,7 +22,7 @@ elif [ "$1" == "1" ]; then
     done
 
 elif [ "$1" == "2" ]; then
-    for r in $(seq 0.10 0.05 0.50); do
+    for r in $(seq 0.20 0.05 0.50); do
         python run_gui_actor.py $1 --r $r --th 0.07 --p 0
         python run_gui_actor.py $1 --r $r --th 0.07 --p 10
         python run_gui_actor.py $1 --r $r --th 0.07 --p 20
@@ -31,7 +32,7 @@ elif [ "$1" == "2" ]; then
     done
 
 elif [ "$1" == "3" ]; then
-    for r in $(seq 0.10 0.05 0.50); do
+    for r in $(seq 0.20 0.05 0.50); do
         python run_gui_actor.py $1 --r $r --th 0.10 --p 0
         python run_gui_actor.py $1 --r $r --th 0.10 --p 10
         python run_gui_actor.py $1 --r $r --th 0.10 --p 20
